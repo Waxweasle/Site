ea740f4ab50f771895ea9a00d702ba748acb2f72
Matthew <103790652+Waxweasle@users.noreply.github.com>
2022-07-20 16:20:22 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -11,10 +10,9 @@ DB_NAME = "database.db"
 def app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
 
     from .views import views
     from .auth import auth
@@ -23,10 +21,11 @@ def app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-    create_db(app)
+
+    create_database(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
+    login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -35,7 +34,8 @@ def app():
 
     return app
 
-def create_db(app):
-    if not path.exists("website/" + DB_NAME):
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
-        print("Database created")
+        print('Databse created')
